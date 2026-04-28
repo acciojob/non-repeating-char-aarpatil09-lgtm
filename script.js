1071fbce570fe6ec38fa6c30818fa3ba0124c5d8
aarpatil09-lgtm <aarpatil09@gmail.com>
2026-04-28 14:35:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	
+	for(let i=0;i<str.length;i++){
+		let new =str[i];
+		for(let k=i+1;k<str.length;k++){
+		if(new==str[k]){
+			return null;
+		}
+	}
+	}
+	return str[i];
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
